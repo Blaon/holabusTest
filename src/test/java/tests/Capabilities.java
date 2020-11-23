1a82df86287a29fa510f36495d422ca47e8a653f
Blaon <iloveelevation@gmail.com>
2020-11-23 13:17:01 +0100

    Create Setup and TearDwon Functions

--- src/test/java/tests/Capabilities.java
+++ src/test/java/tests/Capabilities.java
@@ -1,4 +1,18 @@
 package tests;
 
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+
 public class Capabilities {
+
+
+    @BeforeTest
+    public void setup(){
+
+    }
+
+    @AfterTest
+    public void tearDown(){
+
+    }
 }
